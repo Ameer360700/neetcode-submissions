d859837ac9b6d7eb648347fe3dd1f1b3625daf0a
AMEER SHADAB <163302740+Ameer360700@users.noreply.github.com>
2026-08-04 00:48:53 +0530

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.cpp
@@ -0,0 +1,36 @@
+class Solution {
+
+private:
+    bool ispalindrome(string s, int i, int j)
+    {
+         while(i<=j)
+         {
+             if(s[i]!=s[j])
+             {
+                return false;
+             }
+             i++;
+             j--;
+
+         }
+         return true;
+    }
+public:
+    bool validPalindrome(string s) {
+
+        int n=s.size();
+        int i=0;
+        int j=n-1;
+        while(i<j)
+        {
+            if(s[i]!=s[j])
+            {
+                return(ispalindrome(s,i+1,j) || ispalindrome(s,i,j-1));
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+    }
+};
